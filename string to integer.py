b6f16a8bc5ed4cc474255a7e7e5e2bba57c13a30
Jesinth39 <jesinth39@gamil.com>
2026-07-29 22:20:05 +0530

    String to int

---
+++ string to integer.py
@@ -0,0 +1,35 @@
+class Solution:
+    def myatoi(self, s):
+       INT_MAX = 2**31 - 1
+       INT_MIN = -2**31
+
+        i = 0
+        n = len(s)
+
+        # Step 1: Skip leading whitespaces
+        while i < n and s[i] == ' ':
+            i += 1
+
+        # Step 2: Check sign
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        # Step 3: Convert digits
+        num = 0
+        while i < n and s[i].isdigit():
+            digit = ord(s[i]) - ord('0')
+            num = num * 10 + digit
+            i += 1
+
+        num *= sign
+
+        # Step 4: Clamp to 32-bit signed integer range
+        if num < INT_MIN:
+            return INT_MIN
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
