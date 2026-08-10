9e7ec79d74305439b2227a9c81dda9b030c3ba7b
Jesinth39 <jesinth39@gamil.com>
2026-08-10 21:06:50 +0530

    Expersion

---
+++ Regular Expresions.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isMatch(self, s, p):
+      
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            # Pattern finished
+            if j == len(p):
+                return i == len(s)
+
+            # Check if current characters match
+            first_match = (
+                i < len(s) and
+                (p[j] == s[i] or p[j] == '.')
+            )
+
+            # Handle '*'
+            if j + 1 < len(p) and p[j + 1] == '*':
+                ans = (
+                    dp(i, j + 2) or
+                    (first_match and dp(i + 1, j))
+                )
+            else:
+                ans = first_match and dp(i + 1, j + 1)
+
+            memo[(i, j)] = ans
+            return ans
+
+        return dp(0, 0)
