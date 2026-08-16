4e2db5fa17f8a900410a8428f8540df3abcbe6d2
Jesinth39 <jesinth39@gamil.com>
2026-08-16 20:49:45 +0530

    imported the list file

--- Add two numbers.py
+++ Add two numbers.py
@@ -1,6 +1,9 @@
+from typing import List
+
+
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        dummy = ListNode(0)
+        dummy = List(0)
         current = dummy
         carry = 0
         while l1 or l2 or carry:
@@ -8,7 +11,7 @@ class Solution:
             y =l2.val if l2 else 0
             total = x + y + carry
             carry = total // 10
-            current.next = ListNode(total % 10)
+            current.next = List(total % 10)
             current = current.next
             if l1:
                 l1 = l1.next
