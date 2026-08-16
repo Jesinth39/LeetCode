a5357dbeefd2f33c52f26cbc6e1dbc5a17ba0c87
Jesinth39 <jesinth39@gamil.com>
2026-08-16 20:51:43 +0530

    create another class inside the program

--- Mergeing two sorted list .py
+++ Mergeing two sorted list .py
@@ -1,6 +1,12 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-       
+
         dummy = ListNode(0)
         current = dummy
 
